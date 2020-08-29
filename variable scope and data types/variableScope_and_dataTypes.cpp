fba5a22c5387cf7465a4b3d6ca7b085ee0f63fcc
Ashar <Asharmehmood360@gmail.com>
2020-08-29 07:26:46 -0700

    day1

---
+++ variable scope and data types/variableScope_and_dataTypes.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int glo = 6;
+
+void sum(){
+    cout<<glo;
+}
+
+// local varaiable has higher precedence over global variable
+
+int main(){
+     int glo=9 ;
+    // int a = 3;
+    // int  b = 5;
+    int a = 3, b = 5;
+  //   cout<<"the value of a is "<<a<<". the value of b is "<<b;
+    
+    float pi =3.14;
+  //  cout<< "\n the value of pi is"<<pi;
+
+    char alphabet = 'a';
+  //  cout<< "\n the alphabet is"<<alphabet;
+
+   
+    sum();
+    cout<< glo;
+
+    bool is_true = true;
+   cout<<is_true;
+
+
+    return 0;
+}
---
+++ variables and comments/variables.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+//thsi is a comment
+
+/* this is a 
+multiline comment*/
+
+int main(){
+    int sum = 6;
+    cout<< "hello"<<sum;
+    return 0;
+}
